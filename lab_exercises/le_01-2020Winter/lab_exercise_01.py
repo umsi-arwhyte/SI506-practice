df5f79b1f2a99a1f1c76b58cc2f0750c6b01c87a
Anthony Whyte <anthwhyte@gmail.com>
2020-10-07 08:50:54 -0400

    Add LE01 2020Win

---
+++ lab_exercises/le_01-2020Winter/lab_exercise_01.py
@@ -0,0 +1,36 @@
+# START LAB EXERCISE 01
+
+# The below line will print the string "Lab Exercise 01" to the command line, and then
+# print a new line (which is what the "\n" stands for)
+print('Lab Exercise 01 \n')
+
+
+# PROBLEM 1 (10 points)
+# Replace 'your_name_here' with your name.
+# Replace '100' with the class number.
+# Replace 'your_editor_here' with your editor of choice.
+
+# BEGIN SOLUTION 1
+
+name = "your_name_here" # <- make sure this has quotes around it so that it is a string variable
+class_number = 100 # <- no quotes required here because it is an integer variable
+editor = "your_editor_here" # <- again, make sure this has quotes around it
+
+# END SOLUTION 1
+
+
+# PROBLEM 2 (10 points)
+# The list object named <my_first_list> should contain <name>, <class_number>, and <editor>,
+# in that order.
+
+# BEGIN SOLUTION 2
+
+my_first_list = []
+
+# END SOLUTION 2
+
+# - Don't change any code below this line! - # - # - # - # - # - #
+print(f"My name is {name}.\nI am enrolled in SI {class_number}.\nMy preferred editor is {editor}")
+print(f"My first list:\n{my_first_list}")
+
+# END LAB EXERCISE
